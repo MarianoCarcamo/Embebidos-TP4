f69113e16594dc64bbeb57f198a89a994a63955a
Mariano Carcamo <marianocarcamo98@gmail.com>
2023-04-29 10:41:17 -0300

    Correccion en la creacion de las salidas

--- src/main.c
+++ src/main.c
@@ -139,20 +139,20 @@ int main(void) {
 
     Chip_SCU_PinMuxSet(LED_B_PORT, LED_B_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_B_FUNC);
     
-    led_rgb_azul = DigitalOutputCreate(LED_B_GPIO, LED_B_PIN);
+    led_rgb_azul = DigitalOutputCreate(LED_B_GPIO, LED_B_BIT);
 
     /******************/
     Chip_SCU_PinMuxSet(LED_1_PORT, LED_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_1_FUNC);
     
-    led_rojo = DigitalOutputCreate(LED_1_GPIO, LED_1_PIN);
+    led_rojo = DigitalOutputCreate(LED_1_GPIO, LED_1_BIT);
 
     Chip_SCU_PinMuxSet(LED_2_PORT, LED_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_2_FUNC);
     
-    led_amarillo = DigitalOutputCreate(LED_2_GPIO, LED_2_PIN);
+    led_amarillo = DigitalOutputCreate(LED_2_GPIO, LED_2_BIT);
 
     Chip_SCU_PinMuxSet(LED_3_PORT, LED_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_3_FUNC);
     
-    led_verde = DigitalOutputCreate(LED_3_GPIO,LED_3_PIN);
+    led_verde = DigitalOutputCreate(LED_3_GPIO,LED_3_BIT);
 
     /******************/
     Chip_SCU_PinMuxSet(TEC_1_PORT, TEC_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_1_FUNC);
