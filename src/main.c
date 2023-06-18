d577a576728d0c8a58e3e99b2031ded2bce9427a
Mariano Carcamo <marianocarcamo98@gmail.com>
2023-06-18 20:19:20 -0300

    Reparacion de incremento y decremento BCD

--- src/main.c
+++ src/main.c
@@ -132,23 +132,25 @@ void CambiarModo(modo_t valor) {
 
 void IncrementarBCD(uint8_t numero[2], const uint8_t limite[2]) {
     numero[1]++;
-    if (numero[1] > limite[1]) {
+    if ((numero[0] >= limite[0]) && numero[1] > limite[1]) {
+        numero[0] = 0;
+        numero[1] = 0;
+    }
+    if (numero[1] > 9) {
         numero[1] = 0;
         numero[0]++;
-        if (numero[0] > limite[0]) {
-            numero[0] = 0;
-        }
     }
 }
 
 void DecrementarBCD(uint8_t numero[2], const uint8_t limite[2]) {
     numero[1]--;
-    if (numero[1] > limite[1]) {
-        numero[1] = limite[1];
+    if (numero[1] > 9) {
+        numero[1] = 9;
         numero[0]--;
-        if (numero[0] > limite[0]) {
-            numero[0] = limite[0];
-        }
+    }
+    if ((numero[0] >= limite[0]) && numero[1] >= limite[1]) {
+        numero[0] = limite[0];
+        numero[1] = limite[1];
     }
 }
 
